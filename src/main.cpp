88bb84a8034c625afd19fcf10f4ba4ee998b33ef
Aram-Vn <aramvardanyan13@gmail.com>
2024-01-09 16:58:04 +0400

    STABIL

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <Single_linked_list.h>
+
+int main()
+{
+    my::Forward_list<int> my_list = { 8, 7, 6, 4, 9, 2, 7, 15, 1, 5 };
+
+    my_list.print();
+    return 0;
+
+    
+}
